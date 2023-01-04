2571c113a009762f0be119167974c5cb57898d90
Prateek Takthar <prateektakthar184b@gmail.com>
2023-01-04 20:27:45 +0530

    mid update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import Values from 'values.js'
 
 function App() {
   const [color, setColor] = useState('');
-  const [error, etError] = useState(false);
+  const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
+      setList(colors);
     } catch (error) {
-      
+      setError(true);
+      console.log(error);
     }
   }
   return (
@@ -22,13 +23,16 @@ function App() {
       <section className='container'>
         <h3>Color Finder</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder="#f15025" />
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" className={`${error ? 'error' : null}`} />
           <button className='btn' type='submit'>Submit</button>
         </form>
-        </section>
-        <section className='colors'>
-          <h4>the list goes here</h4>
-        </section>
+      </section>
+      <section className='colors'>
+        {list.map((color,index)=>{
+          console.log(color)
+          return<SingleColor key={index} {...color} index={index} />
+        })}
+      </section>
     </>
   );
 }
