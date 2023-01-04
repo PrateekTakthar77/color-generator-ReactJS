8610a3a0e8fe69c6752b6b45d2b996a84b1d9a17
Prateek Takthar <prateektakthar184b@gmail.com>
2023-01-04 20:49:46 +0530

    "updates"

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = () => {
+  const [alert,setAlert] = useState(false);
+  const bcg = rgb.join(',');
   return <h4>single color</h4>
 }
 
